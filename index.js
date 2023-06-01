0ec440bf3ac45d1e4ec51cf1d4048c36db704f7d
Lucas Vergara <lucasvmsic@gmail.com>
2023-06-01 02:39:18 -0400

    carrito

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ window.sr = ScrollReveal();
         origin: 'botton',
         distance: '-1rem'
     });
+
+    function captura(){
+        var nombre = "dona1";
+        var relleno= document.getElementById("dona1.relleno").value;
+        var cantidad= document.getElementById("dona1.cantidad").value;
+        alert(nombre);
+        alert(relleno);
+        alert(cantidad);
+
+
+    }
+
+
+    var boton = document.getElementById("toggleButton");
+    var overlay = document.getElementById("overlay");
+    
+    boton.addEventListener("click", function() {
+      overlay.style.display = overlay.style.display === "none" ? "block" : "none";
+    });
+    
