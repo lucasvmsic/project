2d9071eafec14af8946a7a9bfca46f56ec93a0f5
Lucas Vergara <lucasvmsic@gmail.com>
2023-06-01 04:53:16 -0400

    carro

--- index.js
+++ index.js
@@ -9,12 +9,17 @@ window.sr = ScrollReveal();
         var nombre = "dona1";
         var relleno= document.getElementById("dona1.relleno").value;
         var cantidad= document.getElementById("dona1.cantidad").value;
-        alert(nombre);
-        alert(relleno);
-        alert(cantidad);
+        location.href ='https://wa.me/56921693746?text=Hola,%20Quiero%20'+cantidad+'%20dona%20'+1+'%20con%20'+relleno;
 
 
     }
+    function cupcake(){
+
+        var relleno1= document.getElementById("cupcake.relleno").value;
+        var cantidad1= document.getElementById("cupcake.cantidad").value;
+        location.href ='https://wa.me/56921693746?text=Hola,%20Quiero%20'+cantidad1+'%20cupcake%20con%20'+relleno1;
+    }
+
 
 
     var boton = document.getElementById("toggleButton");
@@ -24,3 +29,19 @@ window.sr = ScrollReveal();
       overlay.style.display = overlay.style.display === "none" ? "block" : "none";
     });
     
+
+    function cambiarOpacidad() {
+        var elemento = document.querySelector('.elemento');
+        if (elemento.style.opacity === '1') {
+            elemento.style.opacity = '0.5'; // Cambiar la opacidad a 0.5
+        } else {
+            elemento.style.opacity = '1'; // Restaurar la opacidad a 1
+        }
+
+        var overlay = document.getElementById("overlay");
+        if (overlay.style.opacity === '0.8') {
+            overlay.style.opacity = '0.8'; // Cambiar la opacidad a 0.5
+        } else {
+            overlay.style.opacity = '1'; // Restaurar la opacidad a 1
+        }
+        }
