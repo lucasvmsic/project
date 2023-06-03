d5f8419790a3e83d290dfc20f2c05a2f83152d70
Lucas Vergara <lucasvmsic@gmail.com>
2023-06-03 01:57:29 -0400

    hola

--- index.js
+++ index.js
@@ -1,48 +1,68 @@
 window.sr = ScrollReveal();
-    sr.reveal('nav, h1, .card, footer, #carousel', {
-        duration: 1500,
-        origin: 'botton',
-        distance: '-1rem'
-    });
+sr.reveal('nav, h1, .card, footer, #carousel', {
+    duration: 1500,
+    origin: 'botton',
+    distance: '-1rem'
+});
 
-    function captura(){
-        var nombre = "dona1";
-        var relleno= document.getElementById("dona1.relleno").value;
-        var cantidad= document.getElementById("dona1.cantidad").value;
-        location.href ='https://wa.me/56921693746?text=Hola,%20Quiero%20'+cantidad+'%20dona%20'+1+'%20con%20'+relleno;
+function captura() {
+    var nombre = "dona1";
+    var relleno = document.getElementById("dona1.relleno").value;
+    var cantidad = document.getElementById("dona1.cantidad").value;
+    toastr.success("Agregado al carrito");
 
 
+}
+function cupcake() {
+
+    var relleno1 = document.getElementById("cupcake.relleno").value;
+    var cantidad1 = document.getElementById("cupcake.cantidad").value;
+    location.href = 'https://wa.me/56921693746?text=Hola,%20Quiero%20' + cantidad1 + '%20cupcake%20con%20' + relleno1;
+}
+
+
+
+var boton = document.getElementById("toggleButton");
+var overlay = document.getElementById("overlay");
+
+/* boton.addEventListener("mousedown", function() {
+   overlay.style.display = overlay.style.display === "none" ? "block" : "none";
+   console.log("Botón clickeado");
+   console.log("Valor actual de display:", overlay.style.display);
+
+ });
+ */
+let i=0;
+function abrirCarrito() {
+    var elemento = document.querySelector('.all');
+    if(i%2==0){
+    elemento.style.opacity = '0.5';
+    overlay.style.opacity = '1';
+    overlay.style.display = "block";
     }
-    function cupcake(){
-
-        var relleno1= document.getElementById("cupcake.relleno").value;
-        var cantidad1= document.getElementById("cupcake.cantidad").value;
-        location.href ='https://wa.me/56921693746?text=Hola,%20Quiero%20'+cantidad1+'%20cupcake%20con%20'+relleno1;
+    else{
+        elemento.style.opacity = '1';
+        overlay.style.opacity = '0';
+        overlay.style.display = "none";
     }
+    i++;
+}
+  
 
+function agregar() {
 
+    var relleno = document.getElementById("dona2.relleno").value;
+    var cantidad = document.getElementById("dona2.cantidad").value;
+    const listaDeCompras = document.querySelector("#carrito");
+    const li = document.createElement("li");
+    li.textContent = relleno;
+    listaDeCompras.appendChild(li);
+}
+function cerrarCarrito(){
+    var elemento = document.querySelector('.all');
+    elemento.style.opacity = '1';
 
-    var boton = document.getElementById("toggleButton");
-    var overlay = document.getElementById("overlay");
-    
-    boton.addEventListener("click", function() {
-      overlay.style.display = overlay.style.display === "none" ? "block" : "none";
-
-    });
-    
-
-    function cambiarOpacidad() {
-        var elemento = document.querySelector('.all');
-        if (elemento.style.opacity === '1') {
-            elemento.style.opacity = '0.5'; // Cambiar la opacidad a 0.5
-        } else {
-            elemento.style.opacity = '1'; // Restaurar la opacidad a 1
-        }
-
-        var overlay = document.getElementById("overlay");
-        if (overlay.style.opacity === '0.5') {
-            overlay.style.opacity = '1'; // Cambiar la opacidad a 0.5
-        } else {
-            overlay.style.opacity = '0.5'; // Restaurar la opacidad a 1
-        }
-        }
+    overlay.style.opacity = '0';
+    overlay.style.display = "none";
+    i++;
+}
